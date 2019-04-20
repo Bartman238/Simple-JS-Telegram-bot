5f793a1edc4ed383165f0d80abd42e7b31fa4849
Bartmann <Bartman238@gmail.com>
2019-04-21 01:37:41 +0300

    First

---
+++ index.js
@@ -0,0 +1,8 @@
+const Telegraf = require('telegraf');
+const app = new Telegraf ('240240780:AAE9KhlBspRdbgOrSJ4VieMrJ8OczXuUNO0');
+
+app.hears('Привет', ctx => {
+    return ctx.reply('Здарова');
+});
+
+app.startPolling();
