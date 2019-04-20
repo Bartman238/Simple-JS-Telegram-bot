7af8cf015208c7820a9a5cd5d71454dcccf62904
Bartmann <Bartman238@gmail.com>
2019-04-21 01:59:07 +0300

    The Second

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Telegraf = require('telegraf');
-const app = new Telegraf ('240240780:AAE9KhlBspRdbgOrSJ4VieMrJ8OczXuUNO0');
+const app = new Telegraf('240240780:AAE9KhlBspRdbgOrSJ4VieMrJ8OczXuUNO0');
 
 app.hears('Привет', ctx => {
     return ctx.reply('Здарова');
