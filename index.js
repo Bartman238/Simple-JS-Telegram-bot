bada07cf78409eb00cdf88d46ec1aea464ac9549
Bartmann <Bartman238@gmail.com>
2019-04-21 20:11:56 +0300

    Added start status to the log

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const Telegraf = require('telegraf');
-const app = new Telegraf('240240780:AAE9KhlBspRdbgOrSJ4VieMrJ8OczXuUNO0');
+const bot = new Telegraf('240240780:AAE9KhlBspRdbgOrSJ4VieMrJ8OczXuUNO0');
 
-app.hears('Привет', ctx => {
+bot.hears('Привет', ctx => {
     return ctx.reply('Здарова');
 });
 
-app.startPolling();
+bot.telegram.getMe().then((bot_informations) => {
+    bot.options.username = bot_informations.username;
+    console.log(bot_informations.username + " started");
+});
+
+bot.startPolling();
