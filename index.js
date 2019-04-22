294c23d541b90b69a09f23993ff3628d68ab454c
Bartmann <Bartman238@gmail.com>
2019-04-22 11:24:14 +0300

    Now with reply!

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const Telegraf = require('telegraf');
+const Extra = require('telegraf/extra');
 const bot = new Telegraf('240240780:AAE9KhlBspRdbgOrSJ4VieMrJ8OczXuUNO0');
 
 bot.hears('Привет', ctx => {
-    return ctx.reply('Здарова');
+    return ctx.reply('Здарова', Extra.inReplyTo(ctx.message.message_id));
 });
 
 bot.telegram.getMe().then((bot_informations) => {
     bot.options.username = bot_informations.username;
     console.log(bot_informations.username + " started");
 });
+
+bot.command('Test', (ctx) => ctx.reply('HelloWorld'));
+
+bot.hears('Пошел нахуй', (ctx) => ctx.reply('Нет ты'));
 
 bot.startPolling();
