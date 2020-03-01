45faf364bcb6464d4580150947fbb223b0077716
Bartmann <Bartman238@gmail.com>
2020-03-01 04:45:23 +0300

    Добавлен поиск аниме по картинке, произведен рефакторинг кода

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-const Telegraf = require('telegraf');
-const Extra = require('telegraf/extra');
-const config = require('dotenv').config();
-const bot = new Telegraf(process.env.BOT_TOKEN);
+const Telegraf = require('telegraf'),
+    Extra = require('telegraf/extra'),
+    config = require('dotenv').config(),
+    bot = new Telegraf(process.env.BOT_TOKEN),
+    fs = require('fs'),
+    https = require('https'),
+    downloadImg = require('image-downloader'),
+    base64 = require('node-base64-image'),
+    fetch = require('node-fetch');
+
 
 bot.hears('Привет', ctx => {
     return ctx.reply('Здарова', Extra.inReplyTo(ctx.message.message_id));
@@ -12,27 +18,52 @@ bot.telegram.getMe().then((bot_informations) => {
     console.log(bot_informations.username + " started");
 });
 
+bot.hears('Тест', (ctx) => {
+    // console.log(ctx.message);
+    console.log(ctx.message.chat);
+    ctx.telegram.getChatMember(ctx.message.chat.id, ctx.message.from.id).then(res => {
+        console.log(res)
+    });
+})
+
 bot.command('Test', (ctx) => ctx.reply('HelloWorld'));
 
-bot.hears('Тест', (ctx) => ctx.reply('Нет ты'));
+bot.hears('че за аниме?', (ctx)  => {
 
-/*bot.hears('Бот, какое аниме?', (ctx)  => {
-    let msgWithPhoto = ctx.message.reply_to_message.photo[2].file_id;
-    console.log("File ID is " + msgWithPhoto);
-    function searchAnime() {
-        
+    let fileId = ctx.message.reply_to_message.photo;
+
+    if ( fileId ) {
+        ctx.reply(`Хмм, дай-ка подумать...`, Extra.inReplyTo(ctx.message.message_id))
+        ctx.telegram.getFile(fileId[fileId.length - 1].file_id).then(res => {
+            let picLink = `https://api.telegram.org/file/bot${process.env.BOT_TOKEN}/${res.file_path}`;
+
+            base64.encode(picLink, {string: true, local: false}, (err, img) => {
+                fetch("https://trace.moe/api/search", {
+                    method: "POST",
+                    body: JSON.stringify({ image: img }),
+                    headers: { "Content-Type": "application/json" }
+                })
+                    .then(res => res.json())
+                    .then(result => {
+                        if ( result.docs ) {
+                            let anime = result.docs[0];
+                            ctx.reply(`Вероятнее всего, енто ${anime.title_romaji}`, Extra.inReplyTo(ctx.message.message_id))
+                        }
+                    });
+            })
+        })
+    } else {
+        ctx.reply(`Мне нужна картинка`, Extra.inReplyTo(ctx.message.message_id))
     }
-
-    ctx.reply("Это аниме", Extra.inReplyTo(ctx.message.reply_to_message.message_id));
-    
 });
-*/
+
+
 bot.command('roulette', (ctx) => {
     let randomNumber = getRandomNumber(0, 5);
     console.log(ctx.message.from.username + "'s roulette is " + randomNumber);
     if (randomNumber == 0) {
         ctx.reply("Бдыщ! Тебе прострелили руку, и теперь ты не можешь использовать стикосы. [" + randomNumber + "]", Extra.inReplyTo(ctx.message.message_id));
-        //bot.telegram.kickChatMember(ctx.message.chat.id, ctx.message.from.id);
+        //bot.telegram.kickChatMember(ctx.message.chat.id, ctx.message.from.id); // BAN!
         let correctResstrictDate = (Date.now() + 40000)/1000;
         let params = {
             until_date: correctResstrictDate,
@@ -41,20 +72,20 @@ bot.command('roulette', (ctx) => {
             can_send_other_messages: false,
             can_add_web_page_previews: false
         };
-        bot.telegram.restrictChatMember(ctx.message.chat.id, ctx.message.from.id, params);
-        console.log(ctx.message.from.username + " has kicked " + "from " + ctx.message.chat.title);
+        ctx.telegram.getChatMember(ctx.message.chat.id, ctx.message.from.id).then(res => {
+            if (res.status == 'member' && ctx.message.chat.type != 'private') {
+                bot.telegram.restrictChatMember(ctx.message.chat.id, ctx.message.from.id, params);
+                console.log(ctx.message.from.username + " has kicked " + "from " + ctx.message.chat.title);
+            } else {
+                console.log(`Error: ${ctx.message.from.username}'s from ${ctx.message.chat.title} role is ${res.status}`)
+            }
+        });
     } else {
         ctx.reply("Щелк... Тебе повезло. [" + randomNumber + "]", Extra.inReplyTo(ctx.message.message_id));
     }
 });
 
-bot.command('duel', (ctx) => {
-   bot.on('mention', (ctx) => {
-        console.log(ctx.message.text)
-   }); 
-});
-
-function getRandomNumber(min, max) {
+const getRandomNumber = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 };
 
